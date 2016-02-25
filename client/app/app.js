bb721358a7e6da02919d7eda21e6dfe72351cca5
Doug Shamoo <doug.shamoo@gmail.com>
2016-02-24 22:45:09 -0800

    complete ex2

--- client/app/app.js
+++ client/app/app.js
@@ -1,12 +1,17 @@
-/*
- * TODO: import angular and other dependencies
- * like our app.styl and normalize.css and components/home
- * ui-router
- * so the below code works
- */
+// style dependencies
+import 'normalize.css';
+import './app.styl';
+
+// angular dependencies
+import angular from 'angular';
+import uiRouter from 'angular-ui-router';
+import ngAnimate from 'angular-animate';
+
+// components
+import {home} from './components/home/home';
 
 angular.module('app', [
-  // TODO: register other modules here.
-  // remember, this array takes strings
-  // not objects
+  uiRouter,
+  ngAnimate,
+  home.name
 ]);
