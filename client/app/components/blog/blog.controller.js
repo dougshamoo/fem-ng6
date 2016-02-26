b8ee143812653ab51307eb12bdca25e034fb6619
Doug Shamoo <doug.shamoo@gmail.com>
2016-02-25 22:37:56 -0800

    complete ex3

--- client/app/components/blog/blog.controller.js
+++ client/app/components/blog/blog.controller.js
@@ -1,3 +1,14 @@
-// TODO: create the blog controller
-// check blog.html for hints on what the controller
-// should have/do
+import _ from 'lodash';
+
+export class BlogController {
+  constructor() {
+    this.message = 'All sorts of Angular stuff';
+
+    this.posts = _.times(9, i => {
+      return {
+        title: `Post ${i}`,
+        author: `Doug Shamoo`
+      };
+    });
+  }
+}
